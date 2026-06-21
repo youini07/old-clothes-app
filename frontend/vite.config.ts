746d2670013a9cc4228f4a4aa50d93d88dc05abb
youini07 <35138883+youini07@users.noreply.github.com>
2026-06-21 12:05:01 +0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss()],
+    define: {
+          'import.meta.env.VITE_API_URL': JSON.stringify('https://old-clothes-app-production.up.railway.app/api')
+    }
 })
